550f1d286eeb2758816e73b3b80182019a8ce9f2
fersain123-design <fer.sain123@gmail.com>
2026-04-27 20:01:21 +0300

    Add vendor delivery minutes migration

---
+++ prisma/migrations/20260427165752_add_vendor_delivery_minutes/migration.sql
@@ -0,0 +1,6 @@
+﻿-- Add vendor delivery minute columns required by production API queries.
+ALTER TABLE "VendorProfile" ADD COLUMN "deliveryMinMinutes" INTEGER;
+ALTER TABLE "VendorProfile" ADD COLUMN "deliveryMaxMinutes" INTEGER;
+
+CREATE INDEX "VendorProfile_deliveryMinMinutes_idx" ON "VendorProfile"("deliveryMinMinutes");
+CREATE INDEX "VendorProfile_deliveryMaxMinutes_idx" ON "VendorProfile"("deliveryMaxMinutes");
